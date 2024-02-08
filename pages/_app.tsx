72f2bc29458827f037727245dcd3ba526d64ed67
Hiroyuki <hiroyukikumazwa.jp@gmail.com>
2024-02-08 13:34:07 +0000

    update tab space

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,11 @@ import WalletContextProvider from "../contexts/WalletContextProvider"
 import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <WalletContextProvider>
-        <Component {...pageProps} />
-      </WalletContextProvider>
-    </ChakraProvider>
-  )
+    return (
+        <ChakraProvider>
+            <WalletContextProvider>
+                <Component {...pageProps} />
+            </WalletContextProvider>
+        </ChakraProvider>
+    )
 }
--- pages/api/sendTransaction.ts
+++ pages/api/sendTransaction.ts
@@ -1,90 +1,90 @@
 import { Keypair, Transaction } from "@solana/web3.js"
 import type { NextApiRequest, NextApiResponse } from "next"
 import {
-  program,
-  globalLevel1GameDataAccount,
-  connection,
+    program,
+    globalLevel1GameDataAccount,
+    connection,
 } from "../../utils/anchor"
 
 const burner = JSON.parse(process.env.BURNER_KEY ?? "") as number[]
 const burnerKeypair = Keypair.fromSecretKey(Uint8Array.from(burner))
 
 const messages: Record<string, string> = {
-  initialize: "Initializing",
-  moveLeft: "Going left",
-  moveRight: "Going right",
+    initialize: "Initializing",
+    moveLeft: "Going left",
+    moveRight: "Going right",
 };
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  let instruction: string | string[] = req.query.instruction || req.body.instruction;
+    let instruction: string | string[] = req.query.instruction || req.body.instruction;
 
-  if (!instruction) {
-    return res.status(400).json({ message: "Invalid instruction" });
-  }
+    if (!instruction) {
+        return res.status(400).json({ message: "Invalid instruction" });
+    }
 
-  if (Array.isArray(instruction)) {
-    instruction = instruction[0];
-  }
+    if (Array.isArray(instruction)) {
+        instruction = instruction[0];
+    }
 
-  const message = messages[instruction];
+    const message = messages[instruction];
 
-  if (!message) {
-    return res.status(400).json({ message: "Invalid instruction" });
-  }
-  
-  let transaction: Transaction
+    if (!message) {
+        return res.status(400).json({ message: "Invalid instruction" });
+    }
 
-  switch (instruction) {
-    case "initialize":
-      transaction = await program.methods
-        .initialize()
-        .accounts({
-          newGameDataAccount: globalLevel1GameDataAccount,
-          signer: burnerKeypair.publicKey,
-        })
-        .transaction()
-      break
-    case "moveRight":
-      transaction = await program.methods
-        .moveRight()
-        .accounts({
-          gameDataAccount: globalLevel1GameDataAccount,
-        })
-        .transaction()
-      break
-    case "moveLeft":
-      transaction = await program.methods
-        .moveLeft()
-        .accounts({
-          gameDataAccount: globalLevel1GameDataAccount,
-        })
-        .transaction()
-      break
-    default:
-      return res.status(400).json({ message: "Invalid instruction" })
-  }
+    let transaction: Transaction
 
-  const txSig = await connection.sendTransaction(transaction, [burnerKeypair])
-  console.log("Transaction sent:", txSig)
+    switch (instruction) {
+        case "initialize":
+            transaction = await program.methods
+                .initialize()
+                .accounts({
+                    newGameDataAccount: globalLevel1GameDataAccount,
+                    signer: burnerKeypair.publicKey,
+                })
+                .transaction()
+            break
+        case "moveRight":
+            transaction = await program.methods
+                .moveRight()
+                .accounts({
+                    gameDataAccount: globalLevel1GameDataAccount,
+                })
+                .transaction()
+            break
+        case "moveLeft":
+            transaction = await program.methods
+                .moveLeft()
+                .accounts({
+                    gameDataAccount: globalLevel1GameDataAccount,
+                })
+                .transaction()
+            break
+        default:
+            return res.status(400).json({ message: "Invalid instruction" })
+    }
 
-  const { blockhash, lastValidBlockHeight } =
-    await connection.getLatestBlockhash()
-  const confirmation = await connection.confirmTransaction({
-    blockhash,
-    lastValidBlockHeight,
-    signature: txSig,
-  })
+    const txSig = await connection.sendTransaction(transaction, [burnerKeypair])
+    console.log("Transaction sent:", txSig)
 
-  if (!confirmation) {
-    throw new Error("Transaction confirmation failed.")
-  }
+    const { blockhash, lastValidBlockHeight } =
+        await connection.getLatestBlockhash()
+    const confirmation = await connection.confirmTransaction({
+        blockhash,
+        lastValidBlockHeight,
+        signature: txSig,
+    })
 
-  if (message) {
-    res.status(200).json({ message })
-  } else {
-    res.status(400).json({ message: "Invalid request body" })
-  }
+    if (!confirmation) {
+        throw new Error("Transaction confirmation failed.")
+    }
+
+    if (message) {
+        res.status(200).json({ message })
+    } else {
+        res.status(400).json({ message: "Invalid request body" })
+    }
 }
